9b03b44753602dfeeda24b9819780c90821ae588
Erika Leão <leaoerikaleao@pop-os.localdomain>
2021-05-27 18:26:32 -0300

    Code coverage

--- business/files.test.js
+++ business/files.test.js
@@ -9,6 +9,11 @@ const sinon = require('sinon')
 // o teste verifica se a chamada do método está correta e se o 
 // path corresponde ao esperado. 
 describe('reading directory', () => {
+    // volta a fs como era antes, pois 2 stubs estão sendo realizado
+    afterEach(done => {
+        fs.readdir.restore()
+        done()
+    })
     it('reads a directory', () => {
         sinon.stub(fs, 'readdir').callsFake((path, cb) => {
             cb(null, ['file.txt'])
@@ -18,5 +23,17 @@ describe('reading directory', () => {
             expect(list.length).to.equal(1)
             expect(fs.readdir.getCall(0).args[0]).to.equal(path)
         })
+    })
+
+    it('fails reading a directory', () => {
+        sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+            cb('error')
+        })
+        const path = './'
+        return files.readdir(path).
+            catch(err => {
+                expect(err).to.equal('error')
+                expect(fs.readdir.getCall(0).args[0]).to.equal(path)
+            })
     })
 })
